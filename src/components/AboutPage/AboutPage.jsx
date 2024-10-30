45ee5690ed95dfcf2110fb106766b015fcabc025
nick <ndweir13@gmail.com>
2024-10-30 15:44:49 -0500

    images and avatars working

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -8,9 +9,37 @@ import React from 'react';
 function AboutPage() {
   return (
     <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
+      <div className='rankTitles'>
+        <h2>Previous</h2>
+        <h2>Current</h2>
+        <h2>Next</h2>
       </div>
+      
+       <Stack direction="row" spacing={2}>
+      <Avatar
+        alt="Prince"
+        src="https://shorturl.at/sTiy8"
+        sx={{ width: 350, height: 350 }}
+      />
+
+      <Avatar
+        alt="The Grateful Dead"
+        src="https://shorturl.at/zKsMz"
+        sx={{ width: 500, height: 500 }}
+      />
+      <Avatar
+        alt="Enya"
+        src="https://shorturl.at/Xndzz"
+        sx={{ width: 350, height: 350 }}
+      />
+    </Stack>
+
+    <form style={{display: 'flex', justifyContent: 'center'}}>
+      <input placeholder='Rank'></input>
+      <button className='btn'>Submit</button>
+      <button className='btn'>Skip</button>
+    </form>
+
     </div>
   );
 }

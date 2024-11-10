f0b4f622776b5abd4d9f48f7f058e7247fb3af60
nick <ndweir13@gmail.com>
2024-11-10 12:10:33 -0600

    changed data base file

--- database.sql
+++ database.sql
@@ -89,21 +89,50 @@
 -- ALTER TABLE "booking" ADD CONSTRAINT "booking_fk3" FOREIGN KEY ("artist_id") REFERENCES "user"("id");
 -- ALTER TABLE "booking" ADD CONSTRAINT "booking_fk4" FOREIGN KEY ("venue_id") REFERENCES "venues"("id");
 
+-- CREATE TABLE IF NOT EXISTS "user" (
+-- 	"id" bigint NOT NULL,
+-- 	"type" varchar(80) NOT NULL,
+-- 	"username" varchar(80) NOT NULL UNIQUE,
+-- 	"password" varchar(80) NOT NULL UNIQUE,
+-- 	"stage_name" varchar(100),
+-- 	PRIMARY KEY ("id")
+-- );
+
+-- CREATE TABLE IF NOT EXISTS "booking" (
+-- 	"user_id" bigint NOT NULL,
+-- 	"rating" bigint NOT NULL,
+-- 	"artist_id" bigint NOT NULL,
+-- 	"venue_id" bigint NOT NULL
+-- );
+
+
+-- ALTER TABLE "booking" ADD CONSTRAINT "booking_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+
 CREATE TABLE IF NOT EXISTS "user" (
-	"id" bigint NOT NULL,
+	"id" SERIAL PRIMARY KEY,
 	"type" varchar(80) NOT NULL,
 	"username" varchar(80) NOT NULL UNIQUE,
-	"password" varchar(80) NOT NULL UNIQUE,
-	"stage_name" varchar(100),
-	PRIMARY KEY ("id")
+	"password" varchar(80) NOT NULL,
+	"stage_name" varchar(100)
 );
 
 CREATE TABLE IF NOT EXISTS "booking" (
 	"user_id" bigint NOT NULL,
-	"rating" bigint NOT NULL,
-	"artist_id" bigint NOT NULL,
-	"venue_id" bigint NOT NULL
+	"rating" INTEGER NOT NULL,
+	"artist_id" INTEGER DEFAULT NULL,
+	"venue_id" INTEGER DEFAULT NULL
 );
 
 
 ALTER TABLE "booking" ADD CONSTRAINT "booking_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+
+ALTER TABLE "booking"
+ADD "ratingId" SERIAL PRIMARY KEY NOT NULL;
+
+ALTER TABLE "booking" ADD CONSTRAINT unique_user_artist UNIQUE ("user_id", "artist_id");
+ALTER TABLE "booking" ADD CONSTRAINT unique_user_venue UNIQUE ("user_id", "venue_id");
+
+DELETE FROM "booking"
+WHERE user_id = 7 AND artist_id = 3;
+
+ALTER TABLE "user" ALTER COLUMN ("password") varchar(80) NULL;
